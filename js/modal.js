62e795b8bd90a73c085a6a83b09af66567636bc0
IgorManusha <igormanushaviktor@gmail.com>
2024-05-05 20:32:01 +0300

    add scss preprocessors for BEM

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 
-const toggleModal = () => modal.classList.toggle("backdrop-hidden");
+const toggleModal = () => modal.classList.toggle("backdrop__hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
